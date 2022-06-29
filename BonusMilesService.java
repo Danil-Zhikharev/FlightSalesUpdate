4df3b84d7d5f71717a91dac31ba83abd04e8c09b
Danil-Zhikharev <Dane99mail.rugit config --global user.name Danil-Zhikharev$  git config --global user.email Dane99mail.ru>
2022-06-29 23:26:32 +0500

    Last_commit

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,8 +1,10 @@
-public class BonusMilesService {
+public class BonusMilesService{
     int miles;
-    public int calculate(int cost){
-       miles = cost / 20;
+    int price;
+    public int calculate(int price){
+        miles = price / 20;
+        return miles;
     }
-    return miles;
+
 }
 
--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 20;
-        int price = 5000;
-        int miles = price / amount;
-        System.out.println("Начисленные милли: " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
