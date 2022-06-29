3db58dd3eade26208fbe4ff48ff97f32d84b7a83
Danil-Zhikharev <Dane99mail.rugit config --global user.name Danil-Zhikharev$  git config --global user.email Dane99mail.ru>
2022-06-29 23:01:21 +0500

    Last

---
+++ BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    int miles;
+    public int calculate(int cost){
+       miles = cost / 20;
+    }
+    return miles;
+}
+
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;
+        int price = 5000;
+        int miles = price / amount;
+        System.out.println("Начисленные милли: " + miles);
+    }
+}
